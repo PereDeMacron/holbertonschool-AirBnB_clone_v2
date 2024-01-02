a92ba4960f22f025ae1a93053850b8299f3a2cd3
PereDeMacron <peredemacron@gmail.com>
2024-01-02 10:39:56 +0100

    pep8 fixes

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,50 +1,57 @@
 #!/usr/bin/python3
 """Start a Flask web application on localhost"""
-from flask import Flask
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """returns a string"""
-    return("Hello HBNB!")
-
+    """Returns a string: 'Hello HBNB!'"""
+    return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
-    return("HBNB")
-
+    """Returns a string: 'HBNB'"""
+    return "HBNB"
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_print(text):
-    """Function that returns a C(str) and user text"""
+    """
+    Function that returns a string with 'C' followed by the user text.
+    Replaces underscores in the user text with spaces.
+    """
     result = text.replace('_', ' ')
     return "C {}".format(result)
-
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_print(text="is cool"):
-    """Function that returns a Python(str) and user text"""
+    """
+    Function that returns a string with 'Python' followed by the user text.
+    Replaces underscores in the user text with spaces.
+    """
     result = text.replace('_', ' ')
     return "Python {}".format(result)
 
-
 @app.route('/number/<int:n>', strict_slashes=False)
 def print_number(n):
+    """
+    Function that returns a string stating that the provided number is a number.
+    Only works if the provided value is an integer.
+    """
     if type(n) == int:
-        """Function that returns a string only if n is a integer(number)"""
-        return("{} is a number".format(n))
-    
+        return "{} is a number".format(n)
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def print_number(n):
-    if type(n)== int:
-        """Function that displays a HTML page only if n is an integer"""
+def print_number_template(n):
+    """
+    Function that displays an HTML page with the provided number.
+    Only works if the provided value is an integer.
+    Uses the '5-number.html' template for rendering.
+    """
+    if type(n) == int:
         return render_template('5-number.html', n=n)
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=None)
