76e434836111ddbe249f16b980554dd492cd6df3
PereDeMacron <peredemacron@gmail.com>
2024-01-02 10:38:06 +0100

    pep8 fixes

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,58 +1,66 @@
 #!/usr/bin/python3
-"""Start a Flask web application on localhost"""
-from flask import Flask
-from flask import render_template
+"""
+Start a Flask web application on localhost.
+"""
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """returns a string"""
-    return("Hello HBNB!")
-
+    """
+    Returns a string.
+    """
+    return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
-    return("HBNB")
-
+    """
+    Returns HBNB.
+    """
+    return "HBNB"
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_print(text):
-    """Function that returns a C(str) and user text"""
+    """
+    Function that returns a C(str) and user text.
+    """
     result = text.replace('_', ' ')
     return "C {}".format(result)
-
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_print(text="is cool"):
-    """Function that returns a Python(str) and user text"""
+    """
+    Function that returns a Python(str) and user text.
+    """
     result = text.replace('_', ' ')
     return "Python {}".format(result)
 
-
 @app.route('/number/<n>', strict_slashes=False)
 def print_number(n):
-    if type(n)== int:
-        """Function that returns a string only if n is a integer(number)"""
-        return("{} is a number".format(n))
-
+    """
+    Function that returns a string only if n is an integer (number).
+    """
+    if isinstance(n, int):
+        return "{} is a number".format(n)
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def print_number(n):
-    if type(n)== int:
-        """Function that displays a HTML page only if n is an integer"""
+def print_number_template(n):
+    """
+    Function that displays an HTML page only if n is an integer.
+    """
+    if isinstance(n, int):
         return render_template('5-number.html', n=n)
-
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def print_number(n):
-    if type(n)== int:
-        """Function that displays a HTML page only if n is an integer"""
+def print_number_odd_or_even(n):
+    """
+    Function that displays an HTML page only if n is an integer.
+    """
+    if isinstance(n, int):
         return render_template('5-number.html', n=n)
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=None)
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Start a Flask web application on localhost"""
+"""
+Start a Flask web application on localhost.
+"""
 
 from flask import Flask, render_template
 from models import storage
@@ -7,19 +9,20 @@ from models.state import State
 
 app = Flask(__name__)
 
-
 @app.route("/states_list", strict_slashes=False)
 def display_states():
-    """Render state_list html page to display States created"""
+    """
+    Render state_list HTML page to display States created.
+    """
     states = storage.all()
     return render_template('7-states_list.html', states=states)
 
-
 @app.teardown_appcontext
 def teardown(self):
-    """Method to remove current SQLAlchemy Session"""
+    """
+    Method to remove the current SQLAlchemy Session.
+    """
     storage.close()
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
