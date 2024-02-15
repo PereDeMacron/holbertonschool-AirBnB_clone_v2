3af6e541173a2574d23a46ae61b9effcb5918cb6
peredemacron <peredemacron@gmail.com>
2024-02-15 16:02:44 +0100

    fix task 5/9

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -7,12 +7,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
     Returns a string.
     """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
@@ -21,6 +23,7 @@ def hbnb():
     """
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c_print(text):
     """
@@ -28,6 +31,7 @@ def c_print(text):
     """
     result = text.replace('_', ' ')
     return "C {}".format(result)
+
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
@@ -38,6 +42,7 @@ def python_print(text="is cool"):
     result = text.replace('_', ' ')
     return "Python {}".format(result)
 
+
 @app.route('/number/<n>', strict_slashes=False)
 def print_number(n):
     """
@@ -45,6 +50,7 @@ def print_number(n):
     """
     if isinstance(n, int):
         return "{} is a number".format(n)
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def print_number_template(n):
@@ -54,6 +60,7 @@ def print_number_template(n):
     if isinstance(n, int):
         return render_template('5-number.html', n=n)
 
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def print_number_odd_or_even(n):
     """
@@ -61,6 +68,7 @@ def print_number_odd_or_even(n):
     """
     if isinstance(n, int):
         return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=None)
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,6 +9,7 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route("/states_list", strict_slashes=False)
 def display_states():
     """
@@ -17,12 +18,14 @@ def display_states():
     states = storage.all()
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown(self):
     """
     Method to remove the current SQLAlchemy Session.
     """
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
